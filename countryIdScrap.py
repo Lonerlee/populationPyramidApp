48dd593b3495a62f5335511ea136d810f436fab8
Lonerlee <sokalsebastian96@gmail.com>
2023-01-28 22:08:47 +0100

    Adding and using Pprint

--- countryIdScrap.py
+++ countryIdScrap.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import pprint
 
 urlDefault = "https://www.populationpyramid.net/world/2023/"
 
 page = requests.get(urlDefault)
 soup = BeautifulSoup(page.text, 'html.parser')
 
-pageCountry = {0: 'id'}
+headers = ['ID', 'Country']
+
+pageCountry = {}
 
 for country in soup.find_all('a', {'class':"countryLink"}):
     pageCountry[int(country['country'])] = country.text.strip()
 
-print(pageCountry)
+print(headers)
+pprint.pprint(pageCountry)
