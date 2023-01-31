bd2d4951be2f30b0fc7fe2ca62fa1fab9112a484
Lonerlee <sokalsebastian96@gmail.com>
2023-01-31 12:46:23 +0100

    Delting useless code

--- countryIdScrap.py
+++ countryIdScrap.py
@@ -7,19 +7,14 @@ urlDefault = "https://www.populationpyramid.net/world/2023/"
 page = requests.get(urlDefault)
 soup = BeautifulSoup(page.text, 'html.parser')
 
-headers = ['ID', 'Country']
-
 pageCountry = {}
 
 for country in soup.find_all('a', {'class':"countryLink"}):
     pageCountry[country.text.strip().upper()] = int(country['country'])
 
-listShow = ''
-
 listShow = input('Type in Y if you want to see list of avilable or type in anything to not see the list - ').upper()
 
 if listShow == 'Y':
-  print(headers)
   pprint.pprint(pageCountry)
 
 countryID = pageCountry[input('Type in country name from the list - ').upper()]
