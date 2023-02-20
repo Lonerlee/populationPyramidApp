29d23990c381283647b10cec1b7cb759d4f900b5
unknown <sokalsebastian96@gmail.com>
2023-02-20 21:10:15 +0100

    Simplyfying the code

--- countryIdScrap.py
+++ countryIdScrap.py
@@ -1,34 +1,18 @@
-from bs4 import BeautifulSoup
-import requests
-import pprint
+from populationData import *
 
-urlDefault = "https://www.populationpyramid.net/world/2023/"
-
-page = requests.get(urlDefault)
-soup = BeautifulSoup(page.text, 'html.parser')
-
-pageCountry = {}
-
-for country in soup.find_all('a', {'class':"countryLink"}):
-    pageCountry[country.text.strip().upper()] = int(country['country'])
+pageCountry = getCountryList()
 
 listShow = input('Type in Y if you want to see list of avilable or type in any word or key to not see the list - ').upper()
 
 if listShow == 'Y':
-  pprint.pprint(pageCountry)
+  print(pageCountry)
 
 countryName = input('Type in country name from the list - ').upper()
 
-countryID = pageCountry[countryName]
+populationGetData = Population(pageCountry[countryName])
 
-jsonDataURL = "https://www.populationpyramid.net/api/pp/" + str(countryID) + "/2023/"
-jsonWorldData = "https://www.populationpyramid.net/api/pp/900/2023/"
-
-r = requests.get(jsonDataURL)
-rW = requests.get(jsonWorldData)
-
-rawData = r.json()
-rawDataWorld = rW.json()
+rawData = populationGetData.getCountryData()
+rawDataWorld = populationGetData.getWorldData()
 
 print(countryName.capitalize() + ' is approximately a ' + str(round(float(rawData['population'] / rawDataWorld['population']*100), 2)) + '%' + ' of the worlds population')
 
