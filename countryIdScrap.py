a529897ee94dac03ace1698c77c5c472e7e4c02f
unknown <sokalsebastian96@gmail.com>
2023-02-27 12:37:02 +0100

    Testing matplotlib

--- countryIdScrap.py
+++ countryIdScrap.py
@@ -1,5 +1,14 @@
 from populationData import *
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+df = pd.DataFrame({'Age': ['0-4','5-9','10-14','15-19','20-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','60-64','65-69','70-74','75-79','80-84','85-89','90-94','95-99','100+'], 
+                    'Male': [-49228000, -61283000, -64391000, -52437000, -42955000, -44667000, -31570000, -23887000, -22390000, -20971000, -17685000, -15450000, -13932000, -11020000, -7611000, -4653000, -1952000, -625000, -116000, -14000, -1000], 
+                    'Female': [52367000, 64959000, 67161000, 55388000, 45448000, 47129000, 33436000, 26710000, 25627000, 23612000, 20075000, 16368000, 14220000, 10125000, 5984000, 3131000, 1151000, 312000, 49000, 4000, 0]})
+
+AgeClass = ['100+','95-99','90-94','85-89','80-84','75-79','70-74','65-69','60-64','55-59','50-54','45-49','40-44','35-39','30-34','25-29','20-24','15-19','10-14','5-9','0-4']
 
 pageCountry = getCountryList()
 
@@ -42,17 +51,10 @@ femalePrc = round(float(femaleTotal / (maleTotal + femaleTotal) * 100), 2)
 print('There is around ' + str(int(maleTotal)*1000) + ' males in ' + countryName.capitalize() + ' and they make approximately ' + str(malePrc) + '%' + ' of the population')
 print('There is around ' + str(int(femaleTotal)*1000) + ' females in ' + countryName.capitalize() + ' and they make approximately ' + str(femalePrc) + '%' + ' of the population')
 
-fig, ax = plt.subplots()
+bar_plot = sns.barplot(x='Male', y='Age', data=df, order=AgeClass)
 
-prc = [str(malePrc) + '%', str(femalePrc) + '%']
-counts = [malePrc, femalePrc]
-bar_labels = ['Males', 'Females']
-bar_colors = ['tab:red', 'tab:pink']
+bar_plot = sns.barplot(x='Female', y='Age', data=df, order=AgeClass)
 
-ax.bar(prc, counts, label=bar_labels, color=bar_colors)
-
-ax.set_ylabel('% percentage')
-ax.set_title('Percentage of genders in ' + countryName.capitalize())
-ax.legend(title='Genders color')
+bar_plot.set(xlabel="Population (hundreds of millions)", ylabel="Age-Group", title = "Population Pyramid")
 
 plt.show()
