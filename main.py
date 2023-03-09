dfca102953aecc7ed8f2f25f5502fb7b6018afeb
unknown <sokalsebastian96@gmail.com>
2023-03-09 12:43:17 +0100

    Deleting code from maing

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 from populationData import *
 import tkinter as tk
 
-window = tk.Tk()
-label = tk.Label(text="Population Data", width=20, height=0)
-label.pack()
-
-button = tk.Button(
-  text = "Get Data",
-  width = 10,
-  height = 0
-)
-button.pack()
-
-window.mainloop()
